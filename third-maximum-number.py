e6f98308f2a44b441554a3c8d10536b20dc81b02
Nidhisha Shetty <shetty3491@gmail.com>
2021-07-07 23:07:46 +0530

    Create third-maximum-number.py

---
+++ third-maximum-number.py
@@ -0,0 +1,26 @@
+'''
+P.S: Given integer array nums, return the third maximum number in this array. If the third maximum does not exist, return the maximum number.
+'''
+
+#Solution: 
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        arr=[]
+        for x in range(len(nums)):
+            if nums[x] not in arr:
+                arr.append(nums[x])
+        if len(arr)>=3:
+            for x in range(2):
+                arr.remove(max(arr))
+            return max(arr)
+        else:
+            return max(arr)
+ 
+'''
+Complexities:
+Space complexity: O(n)
+'''

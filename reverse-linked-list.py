0a5aee39ea3bdc3ddc1fc07a97d7f04d42189970
Nidhisha Shetty <shetty3491@gmail.com>
2021-07-20 21:28:39 +0530

    Update reverse-linked-list.py

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -3,6 +3,7 @@ P.S: Given the head of a singly linked list, reverse the list, and return the re
 '''
 
 # Solution:
+#using array
 class Solution(object):
     def reverseList(self, head):
         """
@@ -23,3 +24,19 @@ class Solution(object):
         
         return dummy.next
       
+#using linkedlist
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev=None
+        cur=head
+        while cur!=None:
+            nex=cur.next
+            cur.next=prev
+            prev=cur
+            cur=nex
+        head=prev
+        return head

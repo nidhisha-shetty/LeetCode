cd38753eecfb093a2628fcaa32741c10c70a2895
Nidhisha Shetty <shetty3491@gmail.com>
2021-07-20 21:26:12 +0530

    Update palindrome_linked_list.py

--- palindrome_linked_list.py
+++ palindrome_linked_list.py
@@ -3,6 +3,7 @@ P.S: Given the head of a singly linked list, return true if it is a palindrome.
 '''
 
 #Solution:
+#using array
 class Solution(object):
     def isPalindrome(self, head):
         """
@@ -15,3 +16,31 @@ class Solution(object):
             arr.append(head.val)
             head=head.next
         return arr==arr[::-1]
+    
+#using linkedlist
+class Solution(object):
+    def isPalindrome(self, head):
+        
+        prev = ListNode(None,None)
+        curr = head
+        
+        L = 0   # Get Length of Linked list
+        while isinstance(head, ListNode):
+            L+=1
+            head = head.next
+        
+        for _ in range(L//2):
+            nxt_node = curr.next    
+            curr.next = prev    # Reversing first half linkedlist
+            prev = curr     
+            curr = nxt_node
+        
+        left = prev 
+        right = curr if L%2==0 else curr.next
+        while left and right:            
+            if left.val == right.val:
+                left = left.next
+                right = right.next
+            else:
+                return False
+        return True

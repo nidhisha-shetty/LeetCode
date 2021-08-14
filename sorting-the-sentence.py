4429e3c0776eefe1211826db290abe30d9008bef
Nidhisha Shetty <shetty3491@gmail.com>
2021-08-14 21:11:00 +0530

    Update sorting-the-sentence.py

--- sorting-the-sentence.py
+++ sorting-the-sentence.py
@@ -1,11 +1,15 @@
-#solution
+'''
+P.S: Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+"sentence4 a3 is2 This1" -> "This is a sentence"
+'''
+
+#Solution:
 class Solution:
     def sortSentence(self, s: str) -> str:
-        li=s.split(' ')
-        res=['elem']*len(li)
-        for x in range(len(li)):
-            print(x)
-            res[x]=li[x]
-        return res
-        print(li)
-        print(res)
+        li=s.split()
+        li_copy=['a']*len(li)
+        for x in li:
+            li_copy[int(x[-1])-1]=x[:-1]
+        return ' '.join(li_copy)
+            
+

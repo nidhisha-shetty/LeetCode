d0cc8ccb0baf72d0497b79b1586ccf794324a687
Nidhisha Shetty <shetty3491@gmail.com>
2021-08-18 22:45:06 +0530

    Create three-divisors.py

---
+++ three-divisors.py
@@ -0,0 +1,13 @@
+'''
+P.S: Given an integer n, return true if n has exactly three positive divisors. Otherwise, return false.
+An integer m is a divisor of n if there exists an integer k such that n = k * m.
+'''
+
+#Solution:
+class Solution:
+    def isThree(self, n: int) -> bool:
+        sum=0
+        for x in range(1,n+1):
+            if n%x==0:
+                sum+=1
+        return sum==3

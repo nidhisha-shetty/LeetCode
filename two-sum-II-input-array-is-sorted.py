9fecb47224e2db8965e63642f7b1bca3f1febcfb
Nidhisha Shetty <shetty3491@gmail.com>
2021-08-30 21:59:24 +0530

    Update two-sum-II-input-array-is-sorted.py

--- two-sum-II-input-array-is-sorted.py
+++ two-sum-II-input-array-is-sorted.py
@@ -5,8 +5,10 @@ P.S: Given an array of integers numbers that is already sorted in non-decreasing
 #Solution:
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for x in range(len(numbers)):
-            for y in range(len(numbers)):
-                if numbers[x]+numbers[y]==target:
-                    return [x+1, y+1]
+        di={}
+        for x in range(0, len(numbers)):
+            if target-numbers[x] in di:
+                return di[target-numbers[x]], x+1
+            else:
+                di[numbers[x]]=x+1
 

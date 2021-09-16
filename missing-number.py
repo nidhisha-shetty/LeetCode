4c066c8208aa60e07217e8146e447b65dd88ebdc
Nidhisha Shetty <shetty3491@gmail.com>
2021-09-16 22:26:56 +0530

    Update missing-number.py

--- missing-number.py
+++ missing-number.py
@@ -5,13 +5,8 @@ P.S: Given an array nums containing n distinct numbers in the range [0, n], retu
 #Solution:
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        print(nums)
-        min_num=min(nums)
-        max_num=max(nums)
-        for x in range(min_num, max_num):
-            if x not in nums:
-                return x
-        else:
-            if min_num==1:
-                return 0
-            return max_num+1
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i]!=i:
+                return i
+        return len(nums)    

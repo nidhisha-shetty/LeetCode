6aef687098d8b4abb4dff5e4dcc9da5e63f04439
Nidhisha Shetty <nidhisha.shetty@beautosys.com>
2021-09-30 00:22:49 +0530

    Your commit message

---
+++ reverse-prefix-of-word.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        for x in range(len(word)):
+            res=''
+            if word[x]==ch:
+                print(x)
+                res=word[x::-1]+word[x+1:]
+                return res
+                break
+        return word

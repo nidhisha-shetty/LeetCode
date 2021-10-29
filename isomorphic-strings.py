f399986e8f7f50bbb8360a6166ad3b6664de4d96
Nidhisha Shetty <shetty3491@gmail.com>
2021-10-29 23:05:19 +0530

    Create isomorphic-strings.py

---
+++ isomorphic-strings.py
@@ -0,0 +1,20 @@
+'''
+P.S: Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. 
+No two characters may map to the same character, but a character may map to itself.
+'''
+
+#Solution:
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if len(set(s))==len(set(t)):
+            return True
+        else:
+            return False

91aa425996061d19166ed37f19b25de97de536af
Nidhisha Shetty <shetty3491@gmail.com>
2022-03-28 21:44:38 +0530

    Update complex-number-multiplication.py

--- complex-number-multiplication.py
+++ complex-number-multiplication.py
@@ -1,3 +1,13 @@
+'''
+P.S: A complex number can be represented as a string on the form "real+imaginaryi" where:
+
+- real is the real part and is an integer in the range [-100, 100].
+- imaginary is the imaginary part and is an integer in the range [-100, 100].
+- i2 == -1.
+Given two complex numbers num1 and num2 as strings, return a string of the complex number that represents their multiplications.
+'''
+
+#Solution:
 class Solution(object):
     def complexNumberMultiply(self, num1, num2):
         """
@@ -6,5 +16,15 @@ class Solution(object):
         :rtype: str
         """
         num1_split = list(num1.split("+"))
-        res = (int(str(num1_split[0])) ** 2 ) + (2 * (int(str(num1_split[0]))))
-        print(res)
+        num2_split = list(num2.split("+"))
+
+        a = int(num1_split[0])
+        b = int(num1_split[1][0])
+        x = int(num2_split[0])
+        y = int(num2_split[1][0])
+
+        res1 = (a*x)-(b*y)
+        res2 = (a*y)+(b*x)
+        res2 = "+"+str(res2)+"i"
+       
+        return str(res1)+str(res2)

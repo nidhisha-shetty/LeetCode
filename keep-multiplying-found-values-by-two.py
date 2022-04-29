143cbce26773853f8d66e208c3f3db2622945fa3
Nidhisha Shetty <shetty3491@gmail.com>
2022-04-29 23:29:28 +0530

    Create keep-multiplying-found-values-by-two.py

---
+++ keep-multiplying-found-values-by-two.py
@@ -0,0 +1,26 @@
+'''
+P.S: 
+You are given an array of integers nums. You are also given an integer original which is the first number that needs to be searched for in nums.
+
+You then do the following steps:
+
+1. If original is found in nums, multiply it by two (i.e., set original = 2 * original).
+2. Otherwise, stop the process.
+3. Repeat this process with the new number as long as you keep finding the number.
+Return the final value of original.
+'''
+
+#Solution:
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        """
+        :type nums: List[int]
+        :type original: int
+        :rtype: int
+        """
+        if original in nums:
+            pos = nums.index(original) 
+            pos = original * 2
+            original = original * 2
+        else:
+            return original

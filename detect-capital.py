7100898a6a52bf75da80ac3d878bf9c62fdc0c4f
Nidhisha Shetty <shetty3491@gmail.com>
2022-06-03 23:53:09 +0530

    Create detect-capital.py

---
+++ detect-capital.py
@@ -0,0 +1,23 @@
+'''
+P.S: We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+'''
+
+#Solution:
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        count = 0
+        for letter in word:
+            if letter.isupper() and count != 0:
+                if word.isupper():
+                    return True
+                else:
+                    return False
+            else:
+                count+=1
+        return True

85c0f0ade1b23fb0c030cf5807d19a8cbfcb2668
Nidhisha Shetty <shetty3491@gmail.com>
2022-06-20 21:03:54 +0530

    Update first-unique-character-in-a-string.py

--- first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string.py
@@ -5,10 +5,14 @@ Given a string s, find the first non-repeating character in it and return its in
 #Solution:
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        for x in range(len(s)):
-            if s[x] in s[x+1:]:
-                pass
-            else:
-                return x
-        else:
+        di = collections.Counter(s)
+        li = []
+        for x in di:
+            if di[x] == 1:
+                li.append(x)
+        if len(li) == 0:
             return -1
+        else:
+            for x in range(len(s)):
+                if s[x] == li[0]:
+                    return x

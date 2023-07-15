1cbc615d744538c292187362046609bb0a3f4b74
Nidhisha Shetty <shetty3491@gmail.com>
2023-07-15 17:26:46 +0530

    Update longest-subarray-of-1's-after-deleting-one-element.py

--- longest-subarray-of-1's-after-deleting-one-element.py
+++ longest-subarray-of-1's-after-deleting-one-element.py
@@ -8,14 +8,11 @@ class Solution(object):
         curr = 0
         ans = 0
         for x in nums:
-            print(x)
             if x == 1:
                 curr+=1
             else:
                 ans=max(ans, prev+curr)
                 prev=curr
                 curr=0
-                print(ans)
-                print(curr)
         ans=max(ans, prev+curr)
         return ans-1 if ans==len(nums) else ans

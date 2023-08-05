0a248793158dc3a8e1c4f61a4d06b15c41cf81ac
Nidhisha Shetty <shetty3491@gmail.com>
2023-08-05 17:44:22 +0530

    Update merge-strings-alternatively.py

--- merge-strings-alternatively.py
+++ merge-strings-alternatively.py
@@ -1,1 +1,23 @@
-
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        res = ""
+        if len(word1) < len(word2):
+            for x in range(len(word1)):
+                res += word1[x]
+                res += word2[x]
+            res += word2[x+1:]
+        elif len(word1) > len(word2):
+            for x in range(len(word2)):
+                res += word1[x]
+                res += word2[x]
+            res += word1[x+1:]
+        else:
+            for x in range(len(word1)):
+                res += word1[x]
+                res += word2[x]
+        return res

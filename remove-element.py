7352259c258a083d2cf3966c75ab1266aa19126c
Nidhisha Shetty <shetty3491@gmail.com>
2024-01-06 23:59:23 +0530

    Update remove-element.py

--- remove-element.py
+++ remove-element.py
@@ -1,1 +1,16 @@
 
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        index = 0
+        for elem in range(len(nums)):
+            if nums[elem]!=val:
+                nums[index]=nums[elem]
+                index+=1
+        return index
+
+        
